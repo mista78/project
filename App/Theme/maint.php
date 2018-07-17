e61f2eb0eecaf1b356d51ee35bb46c2ede3741e9
Maoulida karim <maoulida.karim78@gmail.com>
2018-07-17 20:46:03 +0200

    sidebar maintenance

--- App/Theme/maint.php
+++ App/Theme/maint.php
@@ -1,3 +1,9 @@
+<?php 
+	
+	$md6 = ['tags' => 'p', 'class' => 'col-md-6'];
+	$md12 = ['tags' => 'p', 'class' => 'col-md-12'];
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,9 +11,10 @@
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
 		<meta charset="utf-8">
 	</head>
+	<?= link_style(['file' => 'css/style']) ?>
 	<?= link_style(['file' => 'css/maint']) ?>
 	<body>
-		<div class="bgimg">
+		<div class="bgimg" style="background-image: url('<?= Webroot("img/maint/" . ($conf['config']['img'] ?? "maintenance.jpg")) ?>');">
 			<div class="middle">
 				<h1><?= @$conf['config']['name_maintenance'] ?? "Coming soon" ?></h1>
 				<hr>
@@ -19,13 +26,62 @@
 				<?php endif ?>
 			</div>
 			<div class="bottomleft">
-			    <p><a href="<?= Url('users/login') ?>" style="color: #fff"><i class="fas fa-fw fa-lock"></i></a></p>
+			    <p><a class="bt" href="<?= Url('users/login') ?>" style="color: #fff"><i class="fas fa-fw fa-lock"></i></a></p>
 			</div>
+			<div id="sidebar" class="sidebar right">
+				<div class="panel">
+					<div class="panel-body">
+						<div class="container">
+							<div class="row">
+							<?= FormStart(['action' => 'users/login','enctype' => 'multipart/form-data']) ?>
+								<?= tags(input('pseudo'," ",['class' => 'form-control']),$md12) ?>
+								<?= tags(input('password'," ",['type' => 'password','class' => 'form-control']),$md12) ?>
+								<?= tags(submit("Se connecter",['class' => 'btn btn-primary m-t-10 btn-block']),$md6) ?>
+								<?= tags(submit("X",['class' => 'btn close btn-primary m-t-10 btn-block']),$md6) ?>
+							<?= FormEnd() ?>
+						</div>
+						</div>
+					</div>
+				</div>
+			</div>	
 			<div class="cover"></div>	
 		</div>
+		
 		<?= link_script(['src' => '//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min']) ?>
 		<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
 		<script>
+
+		const log = n => console.log(n)
+        const btn = document.querySelectorAll('.bt')	
+        const body = document.querySelector('.close')	
+        const resume = document.querySelectorAll('#sidebar')
+        const hide = () => {
+        	resume.forEach((e) => {
+       			e.style.transform = "translateX(" +  e.offsetWidth + "px)"
+       			e.style.height = e.offsetHeight = "px"
+       		})
+        }
+       	hide();
+        btn.forEach((item) => {
+          log(item)
+        	item.addEventListener('click', (e) => {
+        		e.preventDefault()
+        		const btnId = document.querySelector("#sidebar")
+        		
+        		log(btnId)
+        		resume.forEach((e) => {
+        			e.classList.remove('active')
+        		})
+
+        		btnId.classList.add('active')
+        		btnId.style.transform = "translateX(0px)"
+        	})
+        })
+
+        body.addEventListener('click' , (e) => {
+        	e.preventDefault()
+        	hide()
+        })
 		// Set the date we're counting down to
 		var countDownDate = new Date("<?= @$conf['config']['created'] ?? "2019-05-16" ?>").getTime();
 		dated()
